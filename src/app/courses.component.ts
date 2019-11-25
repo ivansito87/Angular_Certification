affb13b20ef9b79f50ba6b6dcf4d75aeccb32b09
Ivan Rendon <ivansito87@gmail.com>
2019-11-25 10:42:21 -0500

    adds solutions for stencil js

---
+++ src/app/courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hi-courses-component',
+  template: '<h2>IT Works!!</h2>'
+})
+export class CoursesComponent {
+    constructor() {
+      
+  }
+}
